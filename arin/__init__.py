cfc4dcaa118d831f7e3e4efb3c4cb94225c8bcb1
Jonathan Senecal <jsenecal@connectitnet.com>
2019-04-26 13:38:50 -0400

    Dont reinvent the wheel

--- arin/__init__.py
+++ arin/__init__.py
@@ -1,5 +1,2 @@
 class PayloadException(Exception):
-    def __init__(self, content):
-        self.content = "Invalid %s" % content
-        # Call the base class constructor with the parameters it needs
-        super().__init__(self.content)
+   pass
